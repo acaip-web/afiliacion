a461958c0055a815300e0a56e408b2edd8faec24
trastoo <132490104+trastoo@users.noreply.github.com>
2026-04-15 00:43:46 +0200

    Add alert to confirm JS link

--- src/scripts.js
+++ src/scripts.js
@@ -1,6 +1,8 @@
 //Confirmando el enlace
 console.log("JS enlazado");
 
+alert("Ahora si");
+
 // Lógica para mostrar el apartado correspondiente
   const radios = document.querySelectorAll('input[name="tipo_personal"]');
   const funcDiv = document.getElementById('grupo-funcionario');
